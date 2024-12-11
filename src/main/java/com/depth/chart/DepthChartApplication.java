a307e104e641997312d850b446354c737fe4f38d
deiyanfott <deiyanfott@yahoo.com>
2024-12-11 20:34:37 +1100

    Implemented sonarlint suggestions

--- src/main/java/com/depth/chart/DepthChartApplication.java
+++ src/main/java/com/depth/chart/DepthChartApplication.java
@@ -2,16 +2,16 @@ package com.depth.chart;
 
 import com.depth.chart.entities.Team;
 import com.depth.chart.repository.TeamRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@AllArgsConstructor
 public class DepthChartApplication {
-    @Autowired
-    private TeamRepository teamRepository;
+    private final TeamRepository teamRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(DepthChartApplication.class, args);
@@ -19,7 +19,7 @@ public class DepthChartApplication {
 
     @Bean
     public CommandLineRunner initialiseTeam() {
-        return (args) -> {
+        return args -> {
             teamRepository.save(Team.of("Tampa Bay Buccaneers"));
             teamRepository.save(Team.of("New York Jets"));
         };
